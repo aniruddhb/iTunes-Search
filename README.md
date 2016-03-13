6346afe4de6c472df63391140c3cb2615bbe2276
Aniruddh Bharadwaj <aniruddh@usc.edu>
2016-03-12 16:28:16 -0800

    Pushed working directory files of iTunes Search to repo.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-**iTunes Search Project Repository for CTC**
+**iTunes Search**
 
-- Made by Aniruddh Bharadwaj
+** Search iTunes for songs, movies, and other media **
+
+- Supports 30-second audio previews given by results["previewUrl"]
+- Supports favorites list with jQuery clickability
+- Supports localStorage favorites list (HTML5 localStorage["favorites"] returns stored favorites)
--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 $(document).ready(function () {
 
+	// on load, hide checkmark
+	$(".favorites-checkmark").hide();
+	$(".favorites-delete").hide();
+
 	// var to keep track of first click (once done, useless)
 	var hasSearched = false;
 
 	// array to hold favorites
 	var favorites = [];
+
+	// on load, parse and add favorites
+	if (JSON.parse(localStorage["favorites_array"])) {
+		loadFavorites();
+	}
+
+	function loadFavorites() {
+		// retrive array
+		favorites = JSON.parse(localStorage["favorites_array"]);
+
+		// push favorites into favorites list
+		for (i = 0; i < favorites.length; i++) {
+			// push favorites into list
+			var favorite = '<p class="favorites-list">' + favorites[i] + '</p>';
+			$(".favorites").append(favorite);
+		}
+	}
 
 	$("#search-button").click(function() {
 
@@ -14,56 +35,104 @@ $(document).ready(function () {
 			dataType: 'jsonp',
 			data: {},
 			url: "https://itunes.apple.com/search?term=" + $("#search-field").val() + "&limit=25",
-			success: function(data) {
+		})
+		.then(function(data) {
 
-				var text = "iTunes Search Results for " + $("#search-field").val();
+			var text = "iTunes Search Results for " + $("#search-field").val();
 
-				if (hasSearched) {
-					$(".results-header").text(text);
-					$(".results-table").remove();
-				} else {
-					// show h2 bar
-					$("html").append($("<p><b></b></p>").addClass("results-header").text(text));
-				} 
+			if (hasSearched) {
+				$(".results-header").text(text);
+				$(".results-table").remove();
+			} else {
+				// show h2 bar
+				$(".content").append("<p><b>" + text + "</b></p>").addClass("results-header");
+			} 
 
-				// create table to show results
-				var table = $("<table></table>").addClass("results-table").addClass("table");
-				// create and append headers
-				var headers = '<tr class="row"><th class="header">Favorite?</th><th class="header">Album Image</th><th class="header">Song Name</th><th class="header">Listen to It</th></tr>'
-				table.append(headers);
+			// create table to show results
+			var table = $("<table></table>").addClass("results-table").addClass("table");
 
-				for (i = 0; i < data.results.length; i++) {
+			// create and append headers
+			var headers = '<tr class="row"><th class="header">Album Image</th><th class="header">Song Name</th><th class="header">Listen to It</th></tr>'
+			table.append(headers);
 
-					// build content for row
+			for (i = 0; i < data.results.length; i++) {
 
-					var img = '<img class="song-img" src="' + data.results[i]['artworkUrl100'] + '">';
-					var name = '<p class="song-name">' + data.results[i]['trackCensoredName'].substring(0, 27) + '</p>';
-					var link = '<a class="song-link" href="' + data.results[i]['trackViewUrl'] + '">' + 
-						data.results[i]['trackViewUrl'].substring(0, 42) + '...' + '</a>'
+				// build content for row
+				var img = '<img class="song-img" id=' + data.results[i]["previewUrl"] + ' src="' + data.results[i]['artworkUrl100'] + '">';
+				var name = '<p class="song-name">' + data.results[i]['trackCensoredName'].substring(0, 27) + '</p>';
+				var link = '<a class="song-link" href="' + data.results[i]['trackViewUrl'] + '">' + 
+					data.results[i]['trackViewUrl'].substring(0, 42) + '...' + '</a>'
 
-					var row = '<tr class="row content_row"><td class="favorite"></td><td>' + img + '</td><td>' + name + '</td><td>'
-						+ link + '</td></tr>';
+				var row = '<tr class="row content-row"><td>' + img + '</td><td>' + name + '</td><td>'
+					+ link + '</td></tr>';
 
-					// append to table
-					table.append(row);
-				}
-
-				// add to html
-				$("html").append(table);
-
-				// set tracker value
-				hasSearched = true;
+				// append to table
+				table.append(row);
 			}
+
+			// add to content div
+			$(".content").append(table);
+
+			// set tracker value
+			hasSearched = true;
+
 		});
 	});
 
-	$(".content-row").on({
-		mouseenter: function () {
-			var checkbox = '<input type="checkbox" class="fav">'
-			$(this).children(".favorite").append(checkbox);
-		},
-		mouseleave: function () {
+	$(".content").on("click", ".song-img", function() {
+		// set audio src
+		$("#thirty-second-preview").attr("src", $(this).attr("id"));
 
+		// play audio
+		$("thirty-second-preview").get(0).play();
+
+	});
+
+	$(".content").on("click", ".song-name", function() {
+		// declare song name
+		var song_name = $(this).text();
+
+		// check if in favorites already
+		if (jQuery.inArray(song_name, favorites) == -1) {
+
+			// if not (-1 return), push into favorites
+			favorites.push(song_name);
+
+			// render on page
+			var favorite = '<p class="favorites-list">' + song_name + '</p>';
+			$(".favorites").append(favorite);
+
+			// fade ins / outs
+			$(".favorites-checkmark").fadeIn();
+			$(".favorites-checkmark").fadeOut();
 		}
+
+		// overhead for pushing array to localstorage
+		localStorage["favorites_array"] = JSON.stringify(favorites);
+	});
+
+	$(".favorites").on("mouseenter mouseleave", ".favorites-list", function(e) {
+		if (e.type == "mouseenter") {
+			$(".favorites-delete").fadeIn();
+		} else {
+			$(".favorites-delete").fadeOut();
+		}
+	});
+
+	$(".favorites").on("click", ".favorites-list", function() {
+		// remove from favorites array
+		favorites = favorites.splice(favorites.indexOf($(this).text()), 1);
+
+		// remove from DOM
+		$(this).remove();
+		$(".favorites-delete").fadeOut();
+
+		// bug fix
+		if (favorites.length == 1) {
+			favorites = [];
+		}
+
+		// set localstorage properly
+		localStorage["favorites_array"] = JSON.stringify(favorites);
 	});
 });
